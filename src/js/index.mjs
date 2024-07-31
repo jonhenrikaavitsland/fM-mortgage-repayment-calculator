98dc91fed4c47138362725c56281b36a10a5665e
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-31 09:57:24 +0200

    issue found

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -8,7 +8,9 @@ export function listenFields() {
   const elements = document.querySelectorAll(`[data-active="input"]`);
   console.log('elements:', elements);
   elements.forEach((element) => {
-    element.classList.remove('border-slate-900');
-    element.classList.add('border-lime');
+    element.addEventListener('click', () => {
+      element.classList.remove('border-slate-900');
+      element.classList.add('border-lime');
+    });
   });
 }
