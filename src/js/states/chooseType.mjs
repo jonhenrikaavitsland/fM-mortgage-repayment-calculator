0a933d97781f1bb1b485e4790207257f3e641600
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-07-31 07:47:37 +0200

    test

--- src/js/states/chooseType.mjs
+++
@@ -1,8 +0,0 @@
-// import { radioListener } from '../listeners/radioListener.mjs';
-
-// export function chooseType() {
-//   const types = document.querySelectorAll(`[data-active="radio"]`);
-//   types.forEach((type) => {
-//     radioListener(type);
-//   });
-// }
